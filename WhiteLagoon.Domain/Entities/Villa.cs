8513c9c6b51ecbd49829168831730469849b465e
Your Name <tiagomixpt@gmail.com>
2024-04-19 14:17:34 +0100

    19/04/2024

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WhiteLagoon.Domain.NewFolder1
+namespace WhiteLagoon.Domain.Entities
 {
     public class Villa
     {
--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WhiteLagoon.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Adicionar ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
